f017199830669c306688e0b041d15a619ee81812
Allegra <aukseallegra@gmail.com>
2018-07-25 11:47:56 +0100

    Done.

--- src/dog.js
+++ src/dog.js
@@ -1,6 +1,6 @@
-class Dog {
-  
-  render() {
-    return `<tr><td>*Name*</td> <td>*Breed*</td> <td>*Sex*</td> <td><button>Edit</button></td></tr>`
-  }
-}
+// class Dog {
+//
+//   render() {
+//     return `<tr><td>*Name*</td> <td>*Breed*</td> <td>*Sex*</td> <td><button>Edit</button></td></tr>`
+//   }
+// }
--- src/dogController.js
+++ src/dogController.js
@@ -1,3 +1,20 @@
-class DogController {
-  
-}
+// class DogController {
+//   static init() {
+//     console.log('dog controller has been run');
+//     Adapter.getDogs()
+//   }
+// }
+
+
+
+
+// class ChoresController {
+//   static init() {
+//     // fetch data
+//     // make chores out of data
+//     // put chores on the DOM
+//     Adapter.getChores()
+//     .then(ChoresController.renderChores)
+//     const form = document.querySelector('#new-chore-form')
+//     form.addEventListener('submit', ChoresController.handleSubmit)
+//   }
