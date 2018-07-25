c864d2d630074565cd1b2b08ef9a05b2568ff118
github username <github email address>
2018-07-25 09:24:45 +0100

    Done.

--- src/dog.js
+++ src/dog.js
@@ -1,6 +1,23 @@
+// class Dog {
+//
+//   render() {
+//     return `<tr><td>*Name*</td> <td>*Breed*</td> <td>*Sex*</td> <td><button>Edit</button></td></tr>`
+//   }
+// }
 class Dog {
-  
+  constructor(id,name,breed,sex){
+    this.id = id
+    this.name = name
+    this.breed = breed
+    this.sex = sex
+  }
+
   render() {
-    return `<tr><td>*Name*</td> <td>*Breed*</td> <td>*Sex*</td> <td><button>Edit</button></td></tr>`
+    return `<tr id="${this.id}">
+    <td>${this.name}</td>
+    <td>${this.breed}</td>
+    <td>${this.sex}</td>
+    <td><button onclick="editDog(${this.id}, '${this.name}', '${this.breed}', '${this.sex}');">Edit</button></td>
+    </tr>`
   }
 }
